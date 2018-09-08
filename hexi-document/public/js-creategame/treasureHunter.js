f5afbf783c4ad991d2b04673aa5297b3de3d80a0
TuanAnhNguyen14111998 <tuananh14111998@gmail.com>
2018-09-08 22:55:45 +0700

    Update hexi-document 3th

--- hexi-document/public/js-creategame/treasureHunter.js
+++ hexi-document/public/js-creategame/treasureHunter.js
@@ -1,297 +1,209 @@
-"use strict";
+// b1: Tao khung hexi va load am nhac
+let g = hexi(512, 512, setup, ["/static/sounds/chimes.wav"]);
 
-/*
-Learn how to use Hexi to build a simple game prototype
-*/
-
-//Create a new Hexi instance, and start it
-var g = hexi(512, 512, setup, ["/static/sounds/chimes.wav"]);
-
-//Set the background color and scale the canvas
-//g.backgroundColor = "black";
+// cho khugn hexi nam giau man hinh dong thoi nen cua man hinh la mau den
 g.scaleToWindow();
 
-//Start Hexi
+// start hexi
 g.start();
 
-//Declare your global variables (global to this game, which means you
-//want to use them in more than one function)
-var dungeon = undefined,
-    player = undefined,
-    treasure = undefined,
-    enemies = undefined,
-    chimes = undefined,
-    exit = undefined,
-    healthBar = undefined,
-    message = undefined,
-    gameScene = undefined,
-    gameOverScene = undefined;
+// dat dinh danh cho cac doi tuong hoat canh se duoc su dung trong tro choi
+let dungeon, player, treasure, enemies, chimes, exit,
+  healthBar, message, gameScene, gameOverScene;
 
-//The `setup` function runs once and is used to initializes your game
+// b2: thiet lap ham setup, ham nay se dc chay mot lan va thich hop cho viec khoi tao cac doi tuong hoat canh
 function setup() {
 
-  //Create the `chimes` sound object
+  // tao doi tuong am nhac
   chimes = g.sound("/static/sounds/chimes.wav");
 
-  //Create the `gameScene` group
+  // tao nhom gameScene
   gameScene = g.group();
 
-  //The exit door
+  // tao ra doi tuong cua thoat
   exit = g.rectangle(48, 48, "green");
   exit.x = 8;
   exit.y = 8;
   gameScene.addChild(exit);
 
-  //The player sprite
+  // tao doi tuong nguoi choi
   player = g.rectangle(32, 32, "blue");
   player.x = 68;
   player.y = g.canvas.height / 2 - player.halfHeight;
   gameScene.addChild(player);
 
-  //Create the treasure
+  // tao kho bau
   treasure = g.rectangle(16, 16, "gold");
 
-  //Position it next to the left edge of the canvas
-  //treasure.x = g.canvas.width - treasure.width - 10;
+  // thiet lap vi tri cho kho bau
   treasure.y = g.canvas.height / 2 - treasure.halfHeight;
 
-  //Alternatively, you could use Ga's built in convience method
-  //called `putCenter` to postion the sprite like this:
+  // dat vi tri can giua cho kho bau
   g.stage.putCenter(treasure, 208, 0);
 
-  //Create a `pickedUp` property on the treasure to help us Figure
-  //out whether or not the treasure has been picked up by the player
+  // thiet lap thuoc tinh de xac lan thu nhat nguoi choi va cham voi kho bau
   treasure.pickedUp = false;
 
-  //Add the treasure to the `gameScene`
+  // them kho bau vao trong nhom gameScene
   gameScene.addChild(treasure);
 
-  //Make the enemies
-  var numberOfEnemies = 6,
-      spacing = 48,
-      xOffset = 150,
-      speed = 2,
-      direction = 1;
+  // tao quai vat
+  let numberOfEnemies = 6,
+    spacing = 48,
+    xOffset = 150,
+    speed = 2,
+    direction = 1;
 
-  //An array to store all the enemies
+  // tao mang quai vat de sau nay duyet qua tung quai vat
   enemies = [];
 
-  //Make as many enemies as there are `numberOfEnemies`
-  for (var i = 0; i < numberOfEnemies; i++) {
+  // tao vong lap de tao 6 quai vat
+  for (let i = 0; i < numberOfEnemies; i++) {
 
-    //Each enemy is a red rectangle
-    var enemy = g.rectangle(32, 32, "red");
+    // tao quai vat mau do
+    let enemy = g.rectangle(32, 32, "red");
 
-    //Space each enemey horizontally according to the `spacing` value.
-    //`xOffset` determines the point from the left of the screen
-    //at which the first enemy should be added.
-    var x = spacing * i + xOffset;
+    // thiet lap vi tri cho quai vat
+    let x = spacing * i + xOffset;
+    let y = g.randomInt(0, g.canvas.height - enemy.height);
 
-    //Give the enemy a random y position
-    var y = g.randomInt(0, g.canvas.height - enemy.height);
-
-    //Set the enemy's direction
     enemy.x = x;
     enemy.y = y;
 
-    //Set the enemy's vertical velocity. `direction` will be either `1` or
-    //`-1`. `1` means the enemy will move down and `-1` means the enemy will
-    //move up. Multiplying `direction` by `speed` determines the enemy's
-    //vertical direction
+    // thiet lap van toc cho quai vat
     enemy.vy = speed * direction;
 
-    //Reverse the direction for the next enemy
+    // chuyen huong cho quai vat tiep theo
     direction *= -1;
 
-    //Push the enemy into the `enemies` array
+    // them quai vat vao trong mang quai vat
     enemies.push(enemy);
 
-    //Add the enemy to the `gameScene`
+    // them quai vat vao trong nhom gameScene
     gameScene.addChild(enemy);
+
   }
 
-  //Create the health bar
-  var outerBar = g.rectangle(128, 16, "black"),
-      innerBar = g.rectangle(128, 16, "yellowGreen");
+  // tao thanh suc khoe
+  let outerBar = g.rectangle(128, 16, "black"),
+    innerBar = g.rectangle(128, 16, "yellowGreen");
 
-  //Group the inner and outer bars
+  // tao group cho hai thanh suc khoe
   healthBar = g.group(outerBar, innerBar);
 
-  //Set the `innerBar` as a property of the `healthBar`
+  // them dinh danh tham chieu den doi tuong innerBar de ve sau cap nhat width cho suc khoe
   healthBar.inner = innerBar;
 
-  //Position the health bar
+  // xac dinh vi tri cho thanh suc khoe
   healthBar.x = g.canvas.width - 148;
   healthBar.y = 16;
 
-  //Add the health bar to the `gameScene`
+  // them thanh suc khoe vao trong gameScene
   gameScene.addChild(healthBar);
 
-  //Add some text for the game over message
+  // them doi tuong text hien thi thong bao ket qua cua tro choi
   message = g.text("Game Over!", "64px Futura", "black", 20, 20);
   message.x = 120;
   message.y = g.canvas.height / 2 - 64;
 
-  //Create a `gameOverScene` group and add the message sprite to it
+  // tao nhom gameOverScene chua doi tuong text
   gameOverScene = g.group(message);
 
-  //Make the `gameOverScene` invisible for now
+  // khong cho gameOverScene hien thi dau tien
   gameOverScene.visible = false;
 
-  //Let the user control the player character using the keyboard.
-  //Hexi's `arrowControl` method lets you do this easily. Supply the
-  //sprite you want to move as the first argument, and the number of
-  //pixels per frame that it should move as the second argument
+  // cho doi tuong nguoi choi duoc di chuyen bang ban phim moi lan di chuyen 5 pixel
   g.arrowControl(player, 5);
 
-  //The `arrowControl` method is great for prototyping a game
-  //but for more flexibility you can also program the arrow keys
-  //manually, like this:
-  /*
-  //Create some keyboard objects using Hexi's `keyboard` method.
-  //You would usually use this code in the `setup` function
-  //Supply the ASCII key code value as the single argument
-  let leftArrow = g.keyboard(37),
-    upArrow = g.keyboard(38),
-    rightArrow = g.keyboard(39),
-    downArrow = g.keyboard(40);
-   //Left arrow key `press` method
-  leftArrow.press = () => {
-    //Change the player's velocity when the key is pressed
-    player.vx = -5;
-    player.vy = 0;
-  };
-  //Left arrow key `release` method
-  leftArrow.release = () => {
-    //If the left arrow has been released, and the right arrow isn't down,
-    //and the player isn't moving vertically:
-    //Stop the player
-    if (!rightArrow.isDown && player.vy === 0) {
-      player.vx = 0;
-    }
-  };
-  upArrow.press = () => {
-    player.vy = -5;
-    player.vx = 0;
-  };
-  upArrow.release = () => {
-    if (!downArrow.isDown && player.vx === 0) {
-      player.vy = 0;
-    }
-  };
-  rightArrow.press = () => {
-    player.vx = 5;
-    player.vy = 0;
-  };
-  rightArrow.release = () => {
-    if (!leftArrow.isDown && player.vy === 0) {
-      player.vx = 0;
-    }
-  };
-  downArrow.press = () => {
-    player.vy = 5;
-    player.vx = 0;
-  };
-  downArrow.release = () => {
-    if (!upArrow.isDown && player.vx === 0) {
-      player.vy = 0;
-    }
-  };
-   */
-
-  //set the game state to `play`
+  // dat trang thai cho hexi la ham play
   g.state = play;
+
 }
 
-//The `play` function contains all the game logic and runs in a loop
+// b3: Tao ham play thuc hien doan code tao cac hieu ung hoat canh trong vong lap tao hoat canh
 function play() {
 
-  //Move the player
+  // cho phep doi tuong nguoi choi co the di chuyen, no tuong tu nhu x + vx, y + vy
   g.move(player);
 
-  //Keep the player contained inside the stage's area
+  // giu cho doi tuong tro choi chi chay trong gioi han cua khung hexi
   g.contain(player, g.stage);
 
-  //Move the enemies and check for a collision
+  // thiet lap thuoc tinh xac dinh nguoi choi da va cham voi quai vat hay chua
+  let playerHit = false;
 
-  //Set `playerHit` to `false` before checking for a collision
-  var playerHit = false;
+  // thuc hien duyet qua vong lap cac quai vat
+  enemies.forEach(enemy => {
 
-  //Loop through all the sprites in the `enemies` array
-  enemies.forEach(function (enemy) {
-
-    //Move the enemy
+    // cho phep doi tuong quai vat co the di chuyen
     g.move(enemy);
 
-    //Check the enemy's screen boundaries
-    var enemyHitsEdges = g.contain(enemy, g.stage);
+    // gioi han doi tuong quai vat khong di chuyen ra ngoai ra khoi khung hexi
+    let enemyHitsEdges = g.contain(enemy, g.stage);
 
-    //If the enemy hits the top or bottom of the stage, reverse
-    //its direction
+    // kiem tra xem doi tuong quai vat cham vao vien tren hay duoi de chuyen doi huong chuyen dong cua no
     if (enemyHitsEdges) {
       if (enemyHitsEdges.has("top") || enemyHitsEdges.has("bottom")) {
         enemy.vy *= -1;
       }
     }
 
-    //Test for a collision. If any of the enemies are touching
-    //the player, set `playerHit` to `true`
+    // kiem tra xem doi tuong nguoi choi co va cham voi doi tuong quai vat hay khong
     if (g.hitTestRectangle(player, enemy)) {
       playerHit = true;
     }
+
   });
 
-  //If the player is hit...
+  // neu co va cham giua nguoi choi va quai vat cho nguoi choi mo di va suc khoe giam 1 pixel neu khong thi hien thi nguoi choi va thanh suc khoe binh thuong
   if (playerHit) {
 
-    //Make the player semi-transparent
     player.alpha = 0.5;
 
-    //Reduce the width of the health bar's inner rectangle by 1 pixel
     healthBar.inner.width -= 1;
+
   } else {
 
-    //Make the player fully opaque (non-transparent) if it hasn't been hit
     player.alpha = 1;
+
   }
 
-  //Check for a collision between the player and the treasure
+  // kiem tra xem doi tuong nguoi choi co va cham voi kho bau hay khong, neu co cho kho bau di theo doi tuong nguoi choi va phat am thanh mot lan dau
   if (g.hitTestRectangle(player, treasure)) {
 
-    //If the treasure is touching the player, center it over the player
     treasure.x = player.x + 8;
     treasure.y = player.y + 8;
 
     if (!treasure.pickedUp) {
 
-      //If the treasure hasn't already been picked up,
-      //play the `chimes` sound
       chimes.play();
       treasure.pickedUp = true;
-    };
+
+    }
+
   }
 
-  //Check for the end of the game
+  // xac dinh khi nao tro choi ket thuc
+  // khi suc khoe het thi hien thi thua
+  if (healthBar.inner.width ==  1) {
 
-  //Does the player have enough health? If the width of the `innerBar`
-  //is less than zero, end the game and display "You lost!"
-  if (healthBar.inner.width < 0) {
     g.state = end;
     message.content = "You lost!";
+
   }
 
-  //If the player has brought the treasure to the exit,
-  //end the game and display "You won!"
+  // neu kho bau duoc va cham voi cua thoat thi hien thi thang cuoc
   if (g.hitTestRectangle(treasure, exit)) {
     g.state = end;
     message.content = "You won!";
   }
+
 }
 
+// phuong thuc trang thai thuc hien code ket thuc tro choi
 function end() {
 
-  //Hide the `gameScene` and display the `gameOverScene`
   gameScene.visible = false;
   gameOverScene.visible = true;
+
 }
-//# sourceMappingURL=treasureHunter.js.map
